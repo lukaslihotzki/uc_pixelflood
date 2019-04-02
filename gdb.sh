b9c6b66f1e802401eb9353a1fc6814c8b844d59b
Lukas Lihotzki <uzwem@student.kit.edu>
2019-04-02 14:52:57 +0200

    Improve GDB script

--- gdb.sh
+++ gdb.sh
@@ -1,2 +1,4 @@
-#!/bin/bash
-exec arm-none-eabi-gdb -iex 'add-auto-load-safe-path .' $1
+#!/bin/sh
+for i in arm-none-eabi-gdb gdb-multiarch; do command -v $i >/dev/null && GDB=$i; done
+[ -n "$GDB" ] || echo "Falling back to standard GDB..."
+exec "${GDB:-gdb}" -iex 'add-auto-load-safe-path .' "$1"
